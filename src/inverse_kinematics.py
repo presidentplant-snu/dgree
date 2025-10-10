3089236b7683d6973b4141ae81e6ad3924c0a3da
michelle3555 <michelle3555@snu.ac.kr>
2025-10-10 11:55:42 +0900

    code

--- src/inverse_kinematics.py
+++ src/inverse_kinematics.py
@@ -1,9 +1,24 @@
 import numpy as np
 
-def inverse_kinematics():
+def inverse_kinematics(target_x, target_y):
+    length1 = 200
+    length2 = 200
+    dist_sq = target_x**2 + target_y**2
+    dist = np.sqrt(dist_sq)
+    
+    cos_theta2 = (dist_sq - length1**2 - length2**2) / (2*length1 * length2)
+    if cos_theta2 > 1:
+        print("no solution")
+    theta2 = np.arccos(cos_theta2)
+    #if elbow up position then negative
 
+    alpha = np.arctan2(target_y, target_x)
+    cos_beta = (length1**2 + dist_sq - length2**2) / (2*length1*dist)
+    beta = np.arccos(cos_beta)
+    theta1 = alpha - beta
+    #if elbow up postion then alpha + beta
 
-    return 
+    return theta1, theta2
 
 if __name__=='__main__':
-    
+    inverse_kinematics(200, 200)
